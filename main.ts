d9361b174c9a894089037fe6904cc7a7e24f74f3
linhcandoit <vutuanlinh02122002@gmail.com>
2024-07-22 11:13:37 +0700

    new commit

--- main.ts
+++ main.ts
@@ -2,7 +2,4 @@ import {exec} from "child_process"
 
 exec("bash run.sh", (error,stdout,stderr) => {
     console.log(stdout)
-    for (let i = 0;i <10;i++){
-        console.log("Hello, now limit")
-    }
 })
